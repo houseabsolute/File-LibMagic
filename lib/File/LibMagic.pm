e1bd4618a0c7ecdfe78dfaeb14a3542f0a2ece10
Dave Rolsky <autarch@urth.org>
2020-03-28 14:28:53 -0500

    Bump version after release

--- lib/File/LibMagic.pm
+++ lib/File/LibMagic.pm
@@ -11,7 +11,7 @@ use File::LibMagic::Constants qw ( constants );
 use Scalar::Util qw( reftype );
 use XSLoader;
 
-our $VERSION = '1.18';
+our $VERSION = '1.19';
 
 XSLoader::load( __PACKAGE__, $VERSION );
 
--- lib/File/LibMagic/Constants.pm
+++ lib/File/LibMagic/Constants.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use Exporter qw( import );
 
-our $VERSION = '1.18';
+our $VERSION = '1.19';
 
 sub constants {
     return qw(
